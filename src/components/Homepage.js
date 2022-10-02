28459e813de4f4c10287dcb3e4e678cc50771baf
Erdinch <erdinchorhan02@gmail.com>
2022-10-02 18:46:42 +0300

    homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,23 +1,31 @@
+import { Link } from 'react-router-dom';
 import '../assets/css/Homepage.css';
 
 const Homepage = () => {
   return (
     <div className="homepage-container">
-      <div className="homepage-title">
-        <h1>
-          Welcome to the online store that <span>delivers</span>
-        </h1>
-      </div>
-      <div className="homepage-content">
-        <div>
-          <p>
-            Only the most <span>high quality</span>
-          </p>
-          <p>fresh</p>
-          <p>
-            fast-fashion <span>clothing</span>
-          </p>
+      <div className="welcome-message">
+        <div className="homepage-title">
+          <h1>
+            Welcome to the online store that <span>delivers</span>
+          </h1>
         </div>
+        <div className="homepage-content">
+          <div>
+            <p>
+              Only the most <span>high quality</span>
+            </p>
+            <p>fresh</p>
+            <p>
+              fast-fashion <span>clothing</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="shop-button">
+        <Link to="/shop">
+          <button>Shop Now</button>
+        </Link>
       </div>
     </div>
   );
