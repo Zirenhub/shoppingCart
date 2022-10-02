c454093c8d94d305d77c3b5e3d1644b7950017e4
Erdinch <erdinchorhan02@gmail.com>
2022-10-02 20:38:30 +0300

    shoping items

--- src/components/Shop-items.js
+++ src/components/Shop-items.js
@@ -1,9 +1,46 @@
+import '../assets/css/Shop-page.css';
+
 const ShopItems = (props) => {
-  const { mensClothing, womensClothing, jewelery } = props;
+  const { items, mensClothing, womensClothing, jewelery } = props;
 
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="shop-container">
+      <div className="sidebar-container">
+        <div>
+          <p>Filters:</p>
+        </div>
+        <div className="sidebar-buttons">
+          <button>Men's Clothing</button>
+          <button>Women's Clothing</button>
+          <button>Jewelery</button>
+        </div>
+      </div>
+      <div className="shop-page-container">
+        {items.map((item) => {
+          return (
+            <div className="shop-item-container" key={item.id}>
+              <div className="main-desc">
+                <div className="image-container">
+                  <img alt="product" src={item.image} />
+                </div>
+                <div className="product-title">
+                  <p>{item.title}</p>
+                </div>
+              </div>
+
+              <div className="product-description">
+                <div className="product-rating">
+                  <p>{item.rating.rate} ⭐</p>
+                  <p>{item.rating.count} reviews</p>
+                </div>
+                <div className="product-price">
+                  <p>{item.price}$</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
