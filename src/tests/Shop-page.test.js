18784ab5bbc810a567beccec55dd9da1181fbb98
Erdinch <erdinchorhan02@gmail.com>
2022-10-03 15:33:19 +0300

    shop page filter test

--- src/tests/Shop-page.test.js
+++ src/tests/Shop-page.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import renderer, { act } from 'react-test-renderer';
 import fetchMock from 'jest-fetch-mock';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import ShopPage from '../components/Shop-page';
 
@@ -15,17 +15,17 @@ describe('Shop page renders', () => {
   });
 });
 
-describe('Adding items', () => {
+describe('Items', () => {
   beforeEach(() => {
     fetch.resetMocks();
   });
 
-  test('Add items', async () => {
+  test('Fetch items', async () => {
     fetch.mockResponse(
       JSON.stringify([
         {
           id: 1,
-          title: 'clothing',
+          title: 'clothing-men-jacket',
           category: "men's clothing",
           rating: {
             rate: 4,
@@ -45,7 +45,7 @@ describe('Adding items', () => {
         },
         {
           id: 3,
-          title: 'clothing',
+          title: 'womens-neckleace',
           category: "women's clothing",
           rating: {
             rate: 4,
@@ -55,7 +55,7 @@ describe('Adding items', () => {
         },
         {
           id: 4,
-          title: 'clothing',
+          title: 'womens-bracelet',
           category: "women's clothing",
           rating: {
             rate: 4,
@@ -103,10 +103,238 @@ describe('Adding items', () => {
           },
           price: 21,
         },
+        {
+          id: 9,
+          title: 'clothing-men-pants',
+          category: "men's clothing",
+          rating: {
+            rate: 1,
+            count: 12,
+          },
+          price: 25,
+        },
       ])
     );
     await act(async () => render(<ShopPage />));
 
     expect(fetch).toHaveBeenCalledTimes(2); // two times because react we are using strict mode ... i think :D
+    expect(screen.getByText('clothing-men-jacket')).toBeInTheDocument();
+    expect(screen.getByText('clothing-men-pants')).toBeInTheDocument();
+    expect(screen.getByText('womens-neckleace')).toBeInTheDocument();
+    expect(screen.getByText('womens-bracelet')).toBeInTheDocument();
+  });
+
+  test('Filter mens items', async () => {
+    fetch.mockResponse(
+      JSON.stringify([
+        {
+          id: 1,
+          title: 'clothing-men-jacket',
+          category: "men's clothing",
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 2,
+          title: 'accessories',
+          category: 'jewelery',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 3,
+          title: 'womens-neckleace',
+          category: "women's clothing",
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 4,
+          title: 'womens-bracelet',
+          category: "women's clothing",
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 5,
+          title: 'electronics',
+          category: 'electronics',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 6,
+          title: 'electronics',
+          category: 'electronics',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 7,
+          title: 'electronics',
+          category: 'electronics',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 8,
+          title: 'electronics',
+          category: 'electronics',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 9,
+          title: 'clothing-men-pants',
+          category: "men's clothing",
+          rating: {
+            rate: 1,
+            count: 12,
+          },
+          price: 25,
+        },
+      ])
+    );
+    await act(async () => render(<ShopPage />));
+    const mensClothingFilter = screen.getByRole('button', {
+      name: "Men's Clothing",
+    });
+
+    userEvent.click(mensClothingFilter);
+    expect(screen.getByText('clothing-men-jacket')).toBeInTheDocument();
+    expect(screen.getByText('clothing-men-pants')).toBeInTheDocument();
+    expect(screen.queryByText('accessories')).not.toBeInTheDocument();
+  });
+
+  test('Filter womens items', async () => {
+    fetch.mockResponse(
+      JSON.stringify([
+        {
+          id: 1,
+          title: 'clothing-men-jacket',
+          category: "men's clothing",
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 2,
+          title: 'accessories',
+          category: 'jewelery',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 3,
+          title: 'womens-neckleace',
+          category: "women's clothing",
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 4,
+          title: 'womens-bracelet',
+          category: "women's clothing",
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 5,
+          title: 'electronics',
+          category: 'electronics',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 6,
+          title: 'electronics',
+          category: 'electronics',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 7,
+          title: 'electronics',
+          category: 'electronics',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 8,
+          title: 'electronics',
+          category: 'electronics',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 9,
+          title: 'clothing-men-pants',
+          category: "men's clothing",
+          rating: {
+            rate: 1,
+            count: 12,
+          },
+          price: 25,
+        },
+      ])
+    );
+    await act(async () => render(<ShopPage />));
+    const womensClothingFilter = screen.getByRole('button', {
+      name: "Women's Clothing",
+    });
+
+    userEvent.click(womensClothingFilter);
+
+    expect(screen.queryByText('clothing-men-jacket')).not.toBeInTheDocument();
+    expect(screen.queryByText('clothing-men-pants')).not.toBeInTheDocument();
+    expect(screen.getByText('womens-neckleace')).toBeInTheDocument();
+    expect(screen.getByText('womens-bracelet')).toBeInTheDocument();
   });
 });
