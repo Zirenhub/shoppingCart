4ef9059fcae4bf4adedfd0648edd9cac77b8c06d
Erdinch <erdinchorhan02@gmail.com>
2022-10-04 18:20:21 +0300

    adding removing displaying items in cart

--- src/tests/Shop-page.test.js
+++ src/tests/Shop-page.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import renderer, { act } from 'react-test-renderer';
 import fetchMock from 'jest-fetch-mock';
 import userEvent from '@testing-library/user-event';
@@ -336,5 +336,157 @@ describe('Items', () => {
     expect(screen.queryByText('clothing-men-pants')).not.toBeInTheDocument();
     expect(screen.getByText('womens-neckleace')).toBeInTheDocument();
     expect(screen.getByText('womens-bracelet')).toBeInTheDocument();
+  });
+});
+
+describe('Adding items to cart', () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+    fetch.mockResponse(
+      JSON.stringify([
+        {
+          id: 1,
+          title: 'clothing-men-jacket',
+          category: "men's clothing",
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 2,
+          title: 'accessories',
+          category: 'jewelery',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 3,
+          title: 'womens-neckleace',
+          category: "women's clothing",
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 4,
+          title: 'womens-bracelet',
+          category: "women's clothing",
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 5,
+          title: 'electronics',
+          category: 'electronics',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 6,
+          title: 'electronics',
+          category: 'electronics',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 7,
+          title: 'electronics',
+          category: 'electronics',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 8,
+          title: 'electronics',
+          category: 'electronics',
+          rating: {
+            rate: 4,
+            count: 222,
+          },
+          price: 21,
+        },
+        {
+          id: 9,
+          title: 'clothing-men-pants',
+          category: "men's clothing",
+          rating: {
+            rate: 1,
+            count: 12,
+          },
+          price: 25,
+        },
+      ])
+    );
+  });
+
+  test('Cart displaying the right amount of items', async () => {
+    await act(async () => render(<ShopPage />));
+
+    const buyMensJacket = screen.getByTestId('clothing-men-jacket');
+    const buyBackpack = screen.getByTestId('clothing-men-pants');
+
+    userEvent.click(buyMensJacket);
+    userEvent.click(buyBackpack);
+
+    const itemsInCart = screen.getByTestId('itemsInCart');
+
+    expect(itemsInCart.textContent).toBe('2');
+  });
+
+  test('Adding products to cart', async () => {
+    await act(async () => render(<ShopPage />));
+
+    const buyMensJacket = screen.getByTestId('clothing-men-jacket');
+    const buyBackpack = screen.getByTestId('clothing-men-pants');
+
+    userEvent.click(buyMensJacket);
+    userEvent.click(buyBackpack);
+
+    const cart = screen.getByTestId('cartBtn');
+
+    userEvent.click(cart);
+
+    expect(screen.getByText('Your items')).toBeInTheDocument();
+    expect(screen.getByTestId('totalCost').textContent).toBe('46$ Total');
+  });
+
+  test('Removing products from cart', async () => {
+    await act(async () => render(<ShopPage />));
+
+    const buyBackpack = screen.getByTestId('clothing-men-pants');
+
+    userEvent.click(buyBackpack);
+
+    const cart = screen.getByTestId('cartBtn');
+
+    userEvent.click(cart);
+
+    const removeButtons = screen.getByRole('button', {
+      name: 'X',
+    });
+
+    userEvent.click(removeButtons);
+
+    expect(screen.getByText('Your items')).toBeInTheDocument();
+    expect(screen.getByTestId('totalCost').textContent).toBe('0$ Total');
   });
 });
